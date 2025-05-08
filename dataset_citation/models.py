87121e98717218c43da59923d26af08d8fcc5b4d
rda-dattore <dattore@ucar.edu>
2025-05-07 19:08:52 -0600

    add dataset_citation from NCAR/rda-web

---
+++ dataset_citation/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from wagtail.core.models import Page
+
+# Create your models here.
+
+
+
+class DatasetCitationPage(Page):
+    dsid = models.CharField(max_length=5, blank=False, default="", unique=True)
+    num_citations = models.IntegerField(blank=False, default=0)
+    citations = models.JSONField(blank=True, default=list)
+
+    is_creatable = False
---
+++ dataset_citation/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from wagtail.core.models import Page
+
+from datasets.utils import ng_gdex_id
+from dataset_description.models import DatasetDescriptionPage
+from dataset_citation.models import DatasetCitationPage
+from home.utils import slug_list
+
+# Create your views here.
+
+
+def citation(request, dsid):
+    dsid = ng_gdex_id(dsid)
+    slist = slug_list(dsid);
+    for slug in slist:
+        qs = Page.objects.type(DatasetCitationPage).filter(url_path__contains=slug).live().specific()
+        if len(qs) > 0:
+            break;
+
+    if len(qs) == 0:
+        return render(request, "404.html")
+
+    d = {'dsid': dsid, 'num_citations': qs[0].num_citations, 'citations': qs[0].citations}
+    if 'HTTP_X_REQUESTED_WITH' in request.META:
+        template = "dataset_citation/dataset_citation.html"
+
+    else:
+        template = "dataset_citation/dataset_citation_page.html"
+        qs2 = Page.objects.type(DatasetDescriptionPage).filter(slug__in=slist).live().specific()
+        if len(qs2) > 0:
+            d.update({'page': qs2[0]})
+            d['page'].dsid = dsid
+
+    return render(request, template, d)
