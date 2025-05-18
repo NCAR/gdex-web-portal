99e70588cfa4a9f42c24d9e6f2e8e9650aab7820
rda-dattore <dattore@ucar.edu>
2025-05-18 08:12:59 -0600

    add oai from NCAR/rda-web

---
+++ oai/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
+
+
+class MetadataFormat(blocks.StructBlock):
+    prefix = blocks.CharBlock(
+            required=True, max_length=30, default="",
+            help_text="Prefix for this metadata format")
+    schema = blocks.CharBlock(
+            required=True, max_length=255, default="",
+            help_text="XML schema for this metadata format")
+    namespace = blocks.CharBlock(
+            required=True, max_length=255, default="",
+            help_text="XML namespace of schema for this metadata format")
+
+
+class MetadataFormatsPage(Page):
+    metadata_formats = StreamField(
+            [('metadata_format', MetadataFormat())],
+            block_counts={'metadata_format': {'min_num': 0}},
+            default="List of exportable metadata formats")
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('metadata_formats', classname="collapsible collapsed")
+    ]
+    is_creatable = False
