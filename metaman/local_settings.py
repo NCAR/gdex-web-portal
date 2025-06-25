84bb09f73c5065890e68302dfa0fbe5e25b2a452
Bob Dattore <dattore@ucar.edu>
2025-06-25 15:54:22 -0600

    Create local_settings.py

---
+++ metaman/local_settings.py
@@ -0,0 +1,16 @@
+metadata_managers = [
+    "<LOGIN_NAME>",
+]
+
+spellcheck_db_config = {
+    'user': "<USERNAME>",
+    'password': "<PASSWORD>",
+    'host': "<HOST>",
+    'dbname': "<DATABASE_NAME>",
+}
+
+rdadata_home = "<PATH_TO_RDADATA_HOME>"
+
+rdadatarun = "<PATH_TO_RDADATARUN>"
+
+doi_manager_auth_key = "<AUTHORIZATION_KEY>"
