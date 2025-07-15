831a2b54f9a5d43e7426a813dc667001ee98bad2
rda-dattore <dattore@ucar.edu>
2025-07-15 15:19:39 -0600

    initial commit

---
+++ gdexwebserver/utils.py
@@ -0,0 +1,19 @@
+iimport os
+import shutil
+import tempfile
+
+
+def make_tempdir():
+    try:
+        tdir_name = tempfile.mkdtemp(dir="/data/ptmp")
+        os.chmod(tdir_name, 0o777)
+        return tdir_name
+    except Exception:
+        return ""
+
+
+def remove_tempdir(tdir_name):
+    try:
+        shutil.rmtree(tdir_name)
+    except Exception:
+        pass
