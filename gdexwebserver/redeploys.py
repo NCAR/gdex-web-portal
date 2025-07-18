d7fefc46cf7b291c8d7a3d1f2a18cc6572a41a9e
rda-dattore <dattore@ucar.edu>
2025-07-17 21:36:07 -0600

    add dsgen and libpkg

--- gdexwebserver/redeploys.py
+++ gdexwebserver/redeploys.py
@@ -3,19 +3,41 @@ import subprocess
 from django.http import HttpResponse
 
 
-def redeploy_spellchecker():
-    o = subprocess.run((
-            "pip install git+https://github.com/NCAR/rda-dsspellchecker; "
-            "/usr/local/gdexweb/bin/dsspellchecker_manage build_db"),
-            shell=True, capture_output=True)
+def respond(o):
     o = (
             o.stdout.decode("utf-8").replace("\n", "<br>") + "<br>" +
             o.stderr.decode("utf-8").replace("\n", "<br>"))
     return HttpResponse(o)
 
 
+def redeploy_dsgen():
+    o = subprocess.run((
+            "pip install git+https://github.com/rda-dattore/testpkg#"
+            "subdirectory=dsgen"), shell=True, capture_output=True)
+    return respond(o)
+
+
+def redeploy_libpkg():
+    o = subprocess.run((
+            "pip install git+https://github.com/rda-dattore/testpkg#"
+            "subdirectory=libpkg"), shell=True, capture_output=True)
+    return respond(o)
+
+
+def redeploy_spellchecker():
+    o = subprocess.run((
+            "pip install git+https://github.com/NCAR/rda-dsspellchecker; "
+            "/usr/local/gdexweb/bin/dsspellchecker_manage build_db"),
+            shell=True, capture_output=True)
+    return respond(o)
+
+
 def redeploy(request, pkg):
-    if pkg == "spellchecker":
+    if pkg == "dsgen":
+        return redeploy_dsgen()
+    elif pkg == "libpkg":
+        return redeploy_libpkg()
+    elif pkg == "spellchecker":
         return redeploy_spellchecker()
 
     return HttpResponse("bad request")
