cf151f96381a704d64394b6ba3ffd8475d729828
rda-dattore <dattore@ucar.edu>
2025-08-14 22:31:33 -0600

    set media directory on PV

--- gdexwebserver/settings/base.py
+++ gdexwebserver/settings/base.py
@@ -242,7 +242,8 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesSto
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join('/data/local/gdexweb', 'media')
 MEDIA_URL = '/media/'
 
 # Caches
