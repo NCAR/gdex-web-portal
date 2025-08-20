c2015a1806811b6b1977dbb190be9e1f3de07854
rda-dattore <dattore@ucar.edu>
2025-08-20 06:33:34 -0600

    convert citation from cgi-bin to Django

---
+++ dataset_citation/export.py
@@ -0,0 +1,135 @@
+import os
+import psycopg2
+
+from django.conf import settings as django_settings
+from django.http import StreamingHttpResponse
+from libpkg.xmlutils import convert_html_to_text
+
+import libpkg.metaformats.settings as metaformat_settings
+
+from .utils import get_authors
+
+
+def get_ds_data(dsid):
+    try:
+        conn = psycopg2.connect(**django_settings.RDADB['metadata_config_pg'])
+        cursor = conn.cursor()
+        cursor.execute((
+                "select s.title, s.summary, s.pub_date, d.doi from search."
+                "datasets as s left join dssdb.dsvrsn as d on d.dsid = s.dsid "
+                "where s.dsid = %s"), (dsid, ))
+        res = cursor.fetchone()
+        return res
+    except Exception as err:
+        raise RuntimeError(err)
+    finally:
+        if 'conn' in locals():
+            conn.close()
+
+
+def get_publisher(ds_title):
+    if ds_title[0:26] == "ICARUS Chamber Experiment:":
+        return metaformat_settings.ARCHIVE['pub_name']['icarus']
+
+    return metaformat_settings.ARCHIVE['pub_name']['default']
+
+
+def create_bibtex(dsid):
+    try:
+        authors = get_authors(dsid)
+        ds_data = get_ds_data(dsid)
+        yield "@misc{{ncar_gdex_dataset_{},\n".format(dsid)
+        yield '  author = "'
+        for index, author in enumerate(authors):
+            if index > 0:
+                yield " and "
+
+            if author[0] == "Person":
+                yield author[1]
+
+                if len(author[2]) > 0:
+                    yield " {}".format(author[2])
+
+                yield " {}".format(author[3])
+            elif author[0] == "Organization":
+                yield " {{{name}}}".format(name=author[1])
+            else:
+                parts = author[1].split(" > ")
+                yield " {{{name}}}".format(name=parts.back())
+
+        yield '",\n'
+        yield "  title = {{{{{title}}}}},\n".format(title=ds_data[0])
+        yield '  publisher = "{}",\n'.format(get_publisher(ds_data[0]))
+        yield '  address = "Boulder, CO",\n'
+        yield "  year = {},\n".format(ds_data[2].year)
+        if len(ds_data[3]) > 0:
+            yield '  doi = "{}"\n'.format(ds_data[3].replace("_", "\\{_}"))
+        else:
+            yield '  url = "{}"\n'.format(
+                    os.path.join(metaformat_settings.ARCHIVE['datasets_url'],
+                                 dsid))
+
+        yield "}\n"
+    except Exception:
+        yield "An error occurred and the file could not be created."
+        return
+
+
+def export_to_bibtex(dsid):
+    headers = {'Content-type': "application/x-bibtex",
+               'Content-disposition': (
+                       "inline; filename=citation-ncar-" + dsid + ".bib")}
+    return StreamingHttpResponse(create_bibtex(dsid), headers=headers)
+
+
+def create_ris(dsid):
+    try:
+        authors = get_authors(dsid)
+        ds_data = get_ds_data(dsid)
+        abstract = convert_html_to_text(
+                "<summary>" + ds_data[1] + "</summary>",
+                wrapLength=80, indentLength=6).replace("\n", "\r\n").lstrip()
+        yield "Provider: {}\r\n".format(
+                metaformat_settings.ARCHIVE['pub_name']['default'])
+        yield "Tagformat: ris\r\n"
+        yield "\r\n"
+        yield "TY  - DATA\r\n"
+        for author in authors:
+            if author[0] == "Person":
+                yield "AU  - {}, {}".format(author[3], author[1])
+                if len(author[2]) > 0:
+                    yield " {}".format(author[2])
+
+                yield "\r\n"
+            elif author[0] == "Organization":
+                yield "AU  - {}\r\n".format(author[1])
+            else:
+                parts = author[1].split(" > ")
+                yield "AU  - {}\r\n".format(parts.back())
+
+        yield "T1  - {}\r\n".format(ds_data[0])
+        yield "AB  - {}\r\n".format(abstract)
+        yield "PY  - {}\r\n".format(ds_data[2].year)
+        if len(ds_data[3]) > 0:
+            yield "DO  - {}\r\n".format(ds_data[3])
+            yield "UR  - {}\r\n".format(
+                    os.path.join(metaformat_settings.DOI_DOMAIN, ds_data[3]))
+        else:
+            yield "UR  - {}\r\n".format(
+                    os.path.join(metaformat_settings.ARCHIVE['datasets_url'],
+                                 dsid))
+
+        yield "PB  - {}\r\n".format(get_publisher(ds_data[0]))
+        yield "CY  - Boulder, CO\r\n"
+        yield "LA  - English\r\n"
+        yield "ER  - \r\n"
+    except Exception:
+        yield "An error occurred and the file could not be created."
+        return
+
+
+def export_to_ris(dsid):
+    headers = {'Content-type': "application/x-research-info-systems",
+               'Content-disposition': (
+                       "inline; filename=citation-ncar-" + dsid + ".ris")}
+    return StreamingHttpResponse(create_ris(dsid), headers=headers)
---
+++ dataset_citation/styles.py
@@ -0,0 +1,144 @@
+import os
+import psycopg2
+
+from string import Template
+from django.conf import settings as django_settings
+from django.http import HttpResponse
+
+import libpkg.metaformats.settings as metaformat_settings
+
+from .export import export_to_bibtex, export_to_ris
+from .utils import list_authors
+
+
+MAP = {
+    'agu': {
+        'max_authors': 9,
+        'other_authors': "et al.",
+        'lfm': "all",
+        'update_frequency': Template(" (Updated $frequency) "),
+        'template': Template((
+                "$authors. ($pub_year). $title $update_frequency[Dataset]. "
+                "$publisher. $url. Accessed$dagger dd mmm yyyy.")),
+    },
+    'ams': {
+        'max_authors': 8,
+        'other_authors': "and Coauthors",
+        'lfm': "first",
+        'template': Template((
+                "$authors, $pub_year: $title. $publisher, accessed$dagger "
+                "dd mm yyyy, $url.")),
+    },
+    'copernicus': {
+        'max_authors': 0,
+        'other_authors': None,
+        'lfm': "all",
+        'template': Template((
+                "$authors: $title, $publisher, $url, $pub_year. Accessed"
+                "$dagger dd mmm yyyy.")),
+    },
+    'datacite': {
+        'max_authors': 0,
+        'other_authors': None,
+        'lfm': "all",
+        'template': Template((
+                "$authors ($pub_year): $title. $publisher. Dataset. $url. "
+                "Accessed$dagger dd mmm yyyy.")),
+    },
+    'esip': {
+        'max_authors': 10,
+        'other_authors': "et al.",
+        'lfm': "first",
+        'update_frequency': Template(", updated $frequency"),
+        'template': Template((
+                "$authors. $pub_year$update_frequency. $title. "
+                "$publisher. $url. Accessed$dagger dd mmm yyyy.")),
+    },
+    'gdj': {
+        'max_authors': 0,
+        'other_authors': None,
+        'lfm': "all",
+        'template': Template((
+                "$authors ($pub_year): $title. $publisher. $url. Accessed"
+                "$dagger dd mmm yyyy.")),
+    },
+}
+
+
+def export_citation(request, dsid, **kwargs):
+    if request.GET['style'] in MAP:
+        try:
+            conn = psycopg2.connect(
+                    **django_settings.RDADB['metadata_config_pg'])
+            cursor = conn.cursor()
+            cursor.execute((
+                    "select title, pub_date, continuing_update from "
+                    "search.datasets where dsid = %s"), (dsid, ))
+            ds_data = cursor.fetchone()
+            e = MAP[request.GET['style']]
+            authors = list_authors(dsid, e['other_authors'],
+                                   maxAuthors=e['max_authors'],
+                                   lastFirstMiddle=e['lfm'])
+            if request.GET['style'] in ("agu", "esip") and authors[-1] == '.':
+                authors = authors[0:-1]
+
+            update_frequency = ""
+            if ds_data[2] == "Y" and 'update_frequency' in e:
+                try:
+                    wconn = psycopg2.connect(
+                            **django_settings.RDADB['wagtail2_config_pg'])
+                    wcursor = wconn.cursor()
+                    wcursor.execute((
+                            "select update_freq from wagtail2."
+                            "dataset_description_datasetdescriptionpage where "
+                            "dsid = %s"), (dsid, ))
+                    res = wcursor.fetchone()
+                    if res is not None:
+                        update_frequency = res[0].lower()
+
+                except Exception:
+                    pass
+                finally:
+                    if 'wconn' in locals():
+                        wconn.close()
+
+            if len(update_frequency) > 0:
+                update_frequency = e['update_frequency'].substitute(
+                                frequency=update_frequency)
+
+            cursor.execute((
+                    "select doi, min(start_date) as sd, max(end_date) as ed, "
+                    "status from dssdb.dsvrsn where dsid = %s and status in "
+                    "('A', 'H') group by doi, status order by sd desc, "
+                    "ed desc"), (dsid, ))
+            dois = cursor.fetchall()
+            if len(dois) > 0:
+                url = os.path.join(metaformat_settings.DOI_DOMAIN,
+                                   dois[0][0])
+            else:
+                url = os.path.join(metaformat_settings.ARCHIVE['datasets_url'],
+                                   dsid, "")
+
+            dagger = '<font color="red">&dagger;</font>'
+            return HttpResponse(
+                    e['template'].substitute(
+                            authors=authors,
+                            pub_year=ds_data[1].year,
+                            update_frequency=update_frequency,
+                            title=ds_data[0],
+                            publisher=metaformat_settings.ARCHIVE[
+                                    'pub_name']['default'],
+                            url=url, dagger=dagger))
+        except Exception as err:
+            #pass
+            return HttpResponse(str(err))
+        finally:
+            if 'conn' in locals():
+                conn.close()
+
+    if request.GET['style'] == "bibtex":
+         return export_to_bibtex(dsid)
+    elif request.GET['style'] == "ris":
+         return export_to_ris(dsid)
+
+    return HttpResponse('<font color="red">Citation unavailable</font>')
