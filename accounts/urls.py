4d4cc2a6f2b0a20abb770d5117f550da2ae25cd8
Thomas Cram <tcram@ucar.edu>
2025-09-24 18:29:35 -0600

    update redirect URL for dashboard in urlpatterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('mylogout', views.logout),
-    path('', RedirectView.as_view(url='/ajax/#!cgi-bin/dashboard')),
+    path('', RedirectView.as_view(url='/dashboard/')),
     path('newtoken', views.newtoken),
 ]
