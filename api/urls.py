d01a2a9814fee476618ecc900971ff4120b3ef7e
Riley Conroy <rpconroy@ucar.edu>
2025-11-11 10:06:56 -0700

    update views

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from dataaccess.views import DataAccessAPIView
 
 urlpatterns = [
     path('paramsummary/<dsid>/', views.param_summary, name='paramsummary'),
