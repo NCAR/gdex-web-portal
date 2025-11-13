b891bf1950c15e1cb5fb96b3c78802f3ec48ce18
Riley Conroy <42748220+rpconroy@users.noreply.github.com>
2025-11-13 09:01:09 -0700

    Riley/swagger feature (#517)
    * Add riley values
    
    * Added workflow for riley
    
    * update image location
    
    * update image
    
    * update requirements
    
    * update workflow
    
    * increment version
    
    * Added API description
    
    * increment version
    
    * stop incrementing version
    
    * Update urls
    
    * update ref
    
    * update ref again
    
    * update
    
    * update test
    
    * update Dockerfile
    
    * update
    
    * update ingress
    
    * update default values
    
    * update views
    
    * corrected indent
    
    * update Dockerfile
    
    * update

--- gdexwebserver/settings/base.py
+++ gdexwebserver/settings/base.py
@@ -90,6 +90,9 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.globus',
     'globus_portal_framework',
     'social_django',
+
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 
@@ -462,3 +465,18 @@ ICOOKIE = local_settings.ICOOKIE
 CSRF_TRUSTED_ORIGINS = ["https://gdex.k8s.ucar.edu", "https://*.ucar.edu"]
 
 LOCAL_API_KEYS = local_settings.LOCAL_API_KEYS
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'RDA Datasets API',
+    'DESCRIPTION': 'Research Data Archive API for accessing and discovering scientific datasets.',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
+
+
