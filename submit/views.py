fe528dc8a71585d40b9795e9d76e22b5db0afb94
Calie Payne <caliepayne@ucar.edu>
2025-11-14 09:43:31 -0700

    Revert "Changed template dir structure"
    This reverts commit 3944143910e3fcfdaa91094a44dc6aeb9019822a.

--- submit/views.py
+++ submit/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def submit_data(request):
-    return render(request,'submit/submit.html', {'display':True})
+    return render(request,'submit.html', {'display':True})
