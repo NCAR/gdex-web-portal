9145df39aa384635b9c43288705f1c2b14f60dee
Riley Conroy <rpconroy@ucar.edu>
2025-12-04 09:35:01 -0700

    fix sytnax

--- home/templatetags/custom_tags.py
+++ home/templatetags/custom_tags.py
@@ -159,7 +159,7 @@ def show_arco_catalogs(context):
                     url = os.path.join(settings.GDEX_SHORT_PATH, 'data', file_info['file_path'].strip('/'))
                 elif '-https' in url:
                     url = settings.GLOBUS_DATA_DOMAIN.strip('/') + file_info['file_path']
-                elif '-osdf' not in url # Some assets don't have posix
+                elif '-osdf' not in url: # Some assets don't have posix
                     url = os.path.join(settings.GDEX_SHORT_PATH, 'data', file_info['file_path'].strip('/'))
                 file_info['file_url'] = url
                 file_info['file_note'] = item.get('note', None)
