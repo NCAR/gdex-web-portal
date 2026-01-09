d28186865b04243d7c37314ac0d5046f1f17c84f
Thomas Cram <tcram@ucar.edu>
2026-01-09 12:13:56 -0700

    fix: change DaasHome to inherit from GDEXPage

--- daas/models.py
+++ daas/models.py
@@ -51,7 +51,7 @@ class DaasForm(AbstractForm):
         return form
 
 
-class DaasHome(Page):
+class DaasHome(GDEXPage):
     # Database fields
     body = RichTextField(blank=True)
     additional_information = RichTextField(blank=True)
