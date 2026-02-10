081f0bd420df4d049fe191806929a5782ea35525
rda-dattore <dattore@ucar.edu>
2026-02-10 15:48:03 -0700

    add some resolver code

--- doi/views.py
+++ doi/views.py
@@ -1,7 +1,17 @@
+import psycopg2
+
+from django.conf import settings as django_settings
 from django.shortcuts import render
 
 
 def resolve(request, doi):
-    return render(request, "400.html")
+    try:
+        conn = psycopg2.connect(**django_settings.RDADB['metadata_config_pg'])
+        cursor = conn.cursor()
+    except Exception as err:
+        return render(request, "doi/doi_page.html", {'error': str(err)})
+    finally:
+        if 'conn' in locals():
+            conn.close()
 
-
+    return render(request, "doi/doi_page.html")
