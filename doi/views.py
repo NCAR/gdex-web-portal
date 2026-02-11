c62b8f7e2476288521f56d48f461724cce2d2206
rda-dattore <dattore@ucar.edu>
2026-02-11 08:25:00 -0700

    add doi query

--- doi/views.py
+++ doi/views.py
@@ -8,6 +8,14 @@ def resolve(request, doi):
     try:
         conn = psycopg2.connect(**django_settings.RDADB['metadata_config_pg'])
         cursor = conn.cursor()
+        cursor.execute((
+                "select dsid, status from dsvrsn where doi ilike %s order by "
+                "status"), (doi, ))
+        res = cursor.fetchone()
+        if res is None:
+            return render(request, "doi/doi_page.html",
+                          {'doi': doi, 'not_gdex': True})
+
     except Exception as err:
         return render(request, "doi/doi_page.html", {'error': str(err)})
     finally:
