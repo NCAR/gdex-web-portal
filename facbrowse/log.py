aa71155e2e4bba1dd20e86767efc95fb6b796b88
Thomas Cram <tcram@ucar.edu>
2026-03-05 14:23:47 -0700

    fix check for hostname

--- facbrowse/log.py
+++ facbrowse/log.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 def add_to_log(message):
     host_parts = socket.gethostname().split(".")
-    if host_parts.find("gdex-webserver") != 0:
+    if "gdex-webserver" in host_parts:
         return
 
     with open("/data/logs/facbrowse_log." + host_parts[0], "a") as f:
