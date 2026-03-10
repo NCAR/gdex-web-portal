9f878bea2e7fe405718ef5a041063cf00c4b1ea2
rda-dattore <dattore@ucar.edu>
2026-03-10 05:55:59 -0600

    add parse_query()

--- csw/views.py
+++ csw/views.py
@@ -14,11 +14,22 @@ def exception(code, **kwargs):
     return d
 
 
+def parse_query(request):
+    if request.method == "GET" and len(request.GET) > 0:
+        pass
+    elif request.method == "POST" and len(request.POST) > 0:
+        pass
+    else:
+        return {'error': {'code': "MIssingParameterValue",
+                          'locator': "REQUEST"}}
+
+
 def respond_to_request(request):
-    if len(request.GET) == 0 and len(request.POST) == 0:
+    csw_request = parse_query(request)
+    if 'error' in csw_request:
+        ctx = exception(csw_request['error']['code'],
+                        locator=csw_request['error']['locator'])
         return render(request, "csw/exception.xml",
-                      context=exception("InvalidParameterValue",
-                                        locator="REQUEST"),
-                      content_type="application/xml", status=400)
+                      context=ctx, content_type="application/xml", status=400)
 
     return render(request, "403.html")
