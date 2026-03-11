965dfcf927d5371a0b9b1e9e9d0c99765a54a7df
rda-dattore <dattore@ucar.edu>
2026-03-11 07:04:23 -0600

    implement Sections

--- csw/views.py
+++ csw/views.py
@@ -51,7 +51,28 @@ def respond_to_request(request):
                       context=ctx, content_type="application/xml", status=400)
 
     if csw_request['request'] == "GetCapabilities":
-        return render(request, "csw/capabilities.xml",
+        if 'sections' in csw_request:
+            sections = csw_request['sections'].split(",")
+            ctx = {}
+            if "ServiceIdentification" in sections:
+                ctx['print_service_identification'] = True
+
+            if "ServiceProvider" in sections:
+                ctx['print_service_provider'] = True
+
+            if "OperationsMetadata" in sections:
+                ctx['print_operations_metadata'] = True
+
+            if "Filter_Capabilities" in sections:
+                ctx['print_filter_capabilities'] = True
+
+        else:
+            ctx = {'print_service_identification': True,
+                   'print_service_provider': True,
+                   'print_operations_metadata': True,
+                   'print_filter_capabilities': True}
+
+        return render(request, "csw/capabilities.xml", context=ctx,
                       content_type="application/xml", status=200)
 
     return render(request, "403.html")
