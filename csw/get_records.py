4f55a13555324190aabcdb42b77332d96ffe6915
rda-dattore <dattore@ucar.edu>
2026-03-19 16:25:57 -0600

    update expires datetime

--- csw/get_records.py
+++ csw/get_records.py
@@ -1,7 +1,6 @@
 import psycopg2
-import pytz
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from django.conf import settings
 from django.shortcuts import render
 from libpkg.metaformats.settings import ARCHIVE
@@ -165,12 +164,12 @@ def summary(request, csw_request, conn):
                         "('P', 'H') and dsid < 'd999000' order by dsid"))
                 res = cursor.fetchall()
                 ctx['num_matched'] = len(res)
+                expires = (datetime.now(tzinfo=timezone.utc) +
+                           timedelta(hours=3))
                 cursor.execute((
                        "insert into metautil.csw_result_set_ids values ("
-                       "%s, %s, %s)"), (ctx['result_set_id'],
-                                       datetime.now(pytz.utc) +
-                                       timedelta(hours=3),
-                                       ctx['num_matched']))
+                       "%s, %s, %s)"), (ctx['result_set_id'], expires,
+                                        ctx['num_matched']))
                 for e in res:
                     cursor.execute((
                             "insert into metautil.csw_result_sets values ("
