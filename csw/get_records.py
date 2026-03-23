24e7a410cb309f019928235a507505dde6c8c4a7
rda-dattore <dattore@ucar.edu>
2026-03-23 15:13:08 -0600

    only process ID parameter if request is GetRecordById

--- csw/get_records.py
+++ csw/get_records.py
@@ -210,7 +210,7 @@ def summary(request, csw_request, conn):
             search_conditions = ["s.type in ('P', 'H')",
                                  "s.dsid < 'd999000'"]
 
-        if 'id' in csw_request:
+        if csw_request['request'] == "GetRecordById" and 'id' in csw_request:
             parts = csw_request['id'].split(",")
             search_conditions.append((
                     """("dc:identifier1" in """ + str(tuple(parts)) + " or "
