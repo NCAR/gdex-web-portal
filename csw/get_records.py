68006d1238551b95f7197bccf86355933044b8c8
rda-dattore <dattore@ucar.edu>
2026-03-23 16:54:23 -0600

    restrict DOIs to status 'A'

--- csw/get_records.py
+++ csw/get_records.py
@@ -171,10 +171,11 @@ def summary(request, csw_request, conn):
 
                     query = (
                             "select s.dsid from search.datasets as s left "
-                            "join dssdb.dsvrsn as v on v.dsid = s.dsid where "
-                            "concat('edu.ucar.gdex:', s.dsid) in " +
-                            str(tuple(parts)) + " or concat('doi:', v.doi) in "
-                            + str(tuple(parts)) + " order by s.dsid")
+                            "join dssdb.dsvrsn as v on v.dsid = s.dsid and "
+                            "v.status = 'A' where concat('edu.ucar.gdex:', "
+                            "s.dsid) in " + str(tuple(parts)) + " or concat("
+                            "'doi:', v.doi) in " + str(tuple(parts)) +
+                            " order by s.dsid")
                 else:
                     query = (
                             "select dsid from search.datasets where type in "
