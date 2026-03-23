128b3b9fe936e5e27b552c2f2af4a8144ca88ca7
rda-dattore <dattore@ucar.edu>
2026-03-23 16:17:40 -0600

    fix indent

--- csw/get_records.py
+++ csw/get_records.py
@@ -169,12 +169,12 @@ def summary(request, csw_request, conn):
                     if len(parts) == 1:
                         parts.append("")
 
-                     query = (
-                             "select s.dsid from search.datasets as s left "
-                             "join dssdb.dsvrsn as v on v.dsid = s.dsid where "
-                             "concat('edu.ucar.gdex:', s.dsid) in " + str("
-                             "tuple(parts)) + " or concat('doi:', v.doi) in "
-                             + str(tuple(parts)) + " order by s.dsid)")
+                    query = (
+                            "select s.dsid from search.datasets as s left "
+                            "join dssdb.dsvrsn as v on v.dsid = s.dsid where "
+                            "concat('edu.ucar.gdex:', s.dsid) in " + str("
+                            "tuple(parts)) + " or concat('doi:', v.doi) in "
+                            + str(tuple(parts)) + " order by s.dsid)")
                 else:
                     query = (
                             "select dsid from search.datasets where type in "
