4aa47a80212e972fe39654b468893519bc0e8e93
rda-dattore <dattore@ucar.edu>
2026-04-14 15:41:06 -0600

    order results by pub year

--- apis/citations/minter.py
+++ apis/citations/minter.py
@@ -33,7 +33,7 @@ def get_dois(minter, cursor, **kwargs):
 def get_publications(minter, cursor, **kwargs):
     query = ("select distinct c.doi_work, w.type, w.pub_year from citation."
              f"data_citations{minter} as c left join citation.works as w on w."
-             "doi = c.doi_work")
+             "doi = c.doi_work order by w.pub_year")
     cursor.execute(query)
     res = cursor.fetchall()
     publications = []
