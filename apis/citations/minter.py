a4dc0e960dbde30ff2c2241c78cec418266a4591
rda-dattore <dattore@ucar.edu>
2026-04-14 15:45:30 -0600

    filter out works with missing info

--- apis/citations/minter.py
+++ apis/citations/minter.py
@@ -33,7 +33,8 @@ def get_dois(minter, cursor, **kwargs):
 def get_publications(minter, cursor, **kwargs):
     query = ("select distinct c.doi_work, w.type, w.pub_year, w.title, w."
              f"publisher from citation.data_citations{minter} as c left join "
-             "citation.works as w on w.doi = c.doi_work order by w.pub_year")
+             "citation.works as w on w.doi = c.doi_work where w.pub_year is "
+             "not null order by w.pub_year")
     cursor.execute(query)
     res = cursor.fetchall()
     publications = []
