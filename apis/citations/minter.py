d738eb1fa069eeddd09d9c089187821c03bb97d5
rda-dattore <dattore@ucar.edu>
2026-04-16 16:26:21 -0600

    add code moved from views.py

--- apis/citations/minter.py
+++ apis/citations/minter.py
@@ -1,5 +1,7 @@
 from . import utils
 
+from lxml import etree
+
 
 def get_dois(minter, cursor, **kwargs):
     query = ("select distinct c.doi_data, d.publisher, d.asset_type from "
@@ -57,3 +59,68 @@ def get_publications(minter, cursor, **kwargs):
                          'published_in': pubdata}})
 
     return (publications, len(res))
+
+
+def update_specific_minter_response(response, minter, cursor, **kwargs):
+    if kwargs['show'] == "dois":
+        dois = get_dois(minter, cursor, **kwargs)
+        if kwargs['output_format'] == ".xml":
+            e = etree.SubElement(response, "dois")
+            for doi in dois:
+                d = etree.SubElement(e, "doi")
+                d.set('ID', doi['doi']['ID'])
+                etree.SubElement(d, "assetType").text = (
+                        doi['doi']['asset-type'])
+
+                etree.SubElement(d, "publisher").text = (
+                        doi['doi']['publisher'])
+
+        else:
+            response['minter'].update({'dois': dois})
+
+        return True
+    elif kwargs['show'] == "publications":
+        publications, citedby_count = get_publications(
+                minter, cursor, **kwargs)
+        if kwargs['output_format'] == ".xml":
+            utils.format_publication_list_as_xml(
+                    publications, citedby_count, response, **kwargs)
+        else:
+            response['minter']['citedby-count'] = citedby_count
+            if kwargs['from_swagger']:
+                response['minter']['comment'] = (
+                       "Example results from this UI are limited to 10 "
+                       "results. A (e.g.) 'curl' command will return all "
+                       "available results.")
+
+            response['minter']['publications'] = publications
+
+        return True
+
+    return False
+
+
+def update_general_minter_response(response, minter, cursor, **kwargs):
+    query = ("select distinct d.publisher, d.asset_type from citation."
+             f"data_citations{minter} as c left join citation.doi_data as d "
+             "on d.DOI_data = c.DOI_data")
+    cursor.execute(query)
+    res = cursor.fetchall()
+    pubs = []
+    assets = []
+    for e in res:
+        pubs.append(e['publisher'])
+        if e['asset_type'] not in assets:
+            assets.append(e['asset_type'])
+
+    if kwargs['output_format'] == ".xml":
+        e = etree.SubElement(response, "assetTypes")
+        for asset in assets:
+            etree.SubElement(e, "assetType").text = asset
+
+        e = etree.SubElement(response, "publishers")
+        for pub in pubs:
+            etree.SubElement(e, "publisher").text = pub
+
+    else:
+        response['minter'].update({'asset-types': assets, 'publishers': pubs})
