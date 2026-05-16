928359e007682602d798a0ea72562f303e341220
Thomas Cram <tcram@ucar.edu>
2026-05-15 19:27:05 -0600

    Update help text for icon_name field in FeaturedCard model to reflect fontawesome version 6 and custom SVG options

--- home/migrations/0008_featuredcard.py
+++ home/migrations/0008_featuredcard.py
@@ -18,11 +18,11 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('sort_order', models.IntegerField(blank=True, editable=False, null=True)),
                 ('title', models.CharField(default='', help_text='Title of the card to be displayed on the home page.', max_length=255, verbose_name='Title')),
-                ('icon_name', models.CharField(default='', help_text='Icon name class from the fontawesome icon set.  See fontawesome version 5 documentation for more info.', max_length=50, verbose_name='Icon')),
+                ('icon_name', models.CharField(default='', help_text='Icon name class from the fontawesome icon set, or custom SVG icomoon icon class.  See fontawesome version 6 documentation for more info.  Custom icomoon icons can be viewed and added to the gdex custom icon set by adding the SVG to the static/unity/lib/icomoon2/ directory and including the icon class name here.', max_length=50, verbose_name='Icon')),
                 ('text', wagtail.fields.RichTextField(default='', help_text='Body text to be displayed on the home page card.', verbose_name='Body Text')),
                 ('card_url', models.URLField(blank=True, help_text='External URL to link to from the card.  If both Card URL and Card Page are provided, Card Page will take precedence.', null=True, verbose_name='Card URL')),
                 ('card_page', models.ForeignKey(
-                    blank=False,
+                    blank=True,
                     help_text='Internal page to link to from the card. If both Card Page and Card URL are provided, Card Page will take precedence.',
                     null=True,
                     on_delete=django.db.models.deletion.SET_NULL,
