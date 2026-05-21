c9eac934498c4baff6cbb4deb6522e394a68a388
rda-dattore <dattore@ucar.edu>
2026-05-21 16:21:51 -0600

    convert parameter codes to text

--- datasets/transform.py
+++ datasets/transform.py
@@ -1,6 +1,8 @@
+import os
 import psycopg2
 
 from functools import cmp_to_key
+from lxml import etree
 
 from django.conf import settings
 from django.http import HttpResponse
@@ -101,6 +103,7 @@ def grml_product_detail(request, dsid, markup_type, time_range_code,
                 "g.grid_definition_code = %s",
                 (file, time_range_code, grid_definition_code))
         res = cursor.fetchall()
+        parameter_maps = {}
         levels = {}
         for e in res:
             level_codes = uncompress_bitmap_values(e[0])
@@ -114,7 +117,19 @@ def grml_product_detail(request, dsid, markup_type, time_range_code,
                 end = [e[3][0:4], "-", e[3][4:6], "-", e[3][6:8], " ",
                        e[3][8:10], ":", e[3][10:12], " +0000"]
                 end = "".join(end)
-                levels[code].append({'name': e[1],
+                pmap, code = e[1].split(":")
+                pmap_key = ".".join([e[5], pmap, "xml"])
+                if pmap_key not in parameter_maps:
+                    parameter_maps[pmap_key] = etree.parse(os.path.join(
+                            "/data/web/metadata/ParameterTables", pmap_key)
+                            ).getroot()
+
+                pdesc = parameter_maps[pmap_key].find(
+                        f"./parameter[@code={code}]/description")
+                if pdesc is None:
+                    pdesc = e[1]
+
+                levels[code].append({'name': pdesc,
                                      'datetime_range': " to ".join(
                                              [start, end]),
                                      'num_grids': e[4]})
