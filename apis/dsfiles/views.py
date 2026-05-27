5bf795923635f1a5c312b1855c453756865697d7
rda-dattore <dattore@ucar.edu>
2026-05-27 16:47:21 -0600

    change 'selections' to 'filters'

--- apis/dsfiles/views.py
+++ apis/dsfiles/views.py
@@ -1,5 +1,16 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def swagger(request, output=None):
     return render(request, "dsfiles/swagger.html", {})
+
+
+def filters(request, dsid):
+    response = {'DSID': dsid,
+                'filters': {
+                        'valid-dates': {
+                                'min': "", 'max': ""}}}
+    return HttpResponse(json.dumps(response))
