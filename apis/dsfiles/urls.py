b8c15eed27196ed161a287b1c2c1d852b1ec7dd1
rda-dattore <dattore@ucar.edu>
2026-06-02 06:06:21 -0600

    update path

--- apis/dsfiles/urls.py
+++ apis/dsfiles/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 
 urlpatterns = [
     path('', views.swagger),
-    path('<dsid>/<operation>/', views.respond_to_request),
+    re_path(r'^(?P<dsid>d[0-9]{6})/(?P<operation>([^/]*){1})/(?P<data_type>([^/]*){0,})$', views.respond_to_request),
 ]
