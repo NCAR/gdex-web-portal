c4de6d28e52c1ef35ab0dc739ce86a70cdef68a7
rda-dattore <dattore@ucar.edu>
2026-06-03 16:28:57 -0600

    fix path for datatypes

--- apis/dsfiles/urls.py
+++ apis/dsfiles/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 
 urlpatterns = [
     path('', views.swagger),
-    path('<dsid>/datatypes/', views.respond_to_request),
+    re_path(r'^(?P<dsid>d[0-9]{6})/(?P<operation>datatypes)/$', views.respond_to_request),
     path('<dsid>/<operation>/<datatype>/', views.respond_to_request),
 ]
