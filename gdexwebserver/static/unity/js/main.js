a16566a06ed6e29278b30e0c27c585ff48f22c7d
chrisgadze <gadzechris001@gmail.com>
2026-07-13 12:51:31 -0600

    Enhance dropdown menu functionality with improved hover behavior and close delay

--- gdexwebserver/static/unity/js/main.js
+++ gdexwebserver/static/unity/js/main.js
@@ -106,7 +106,16 @@
   }
 
   function enableNavDropdownHover() {
+    var HOVER_CLOSE_DELAY = 300
     var navDropdowns = document.querySelectorAll('.navbar .nav-item.dropdown')
+
+    function isDesktop() {
+      return window.innerWidth >= 1024
+    }
+
+    function getDropdown(toggle) {
+      return bootstrap.Dropdown.getOrCreateInstance(toggle)
+    }
 
     navDropdowns.forEach(function (item) {
       var toggle = item.querySelector('.dropdown-toggle')
@@ -115,20 +124,27 @@
       var menu = item.querySelector('.dropdown-menu')
       var closeTimer
 
+      function closeOtherDropdowns() {
+        navDropdowns.forEach(function (other) {
+          var otherToggle = other.querySelector('.dropdown-toggle')
+          if (other !== item && otherToggle) getDropdown(otherToggle).hide()
+        })
+      }
+
       function openMenu() {
         clearTimeout(closeTimer)
-        if (window.innerWidth >= 1024) {
-          bootstrap.Dropdown.getOrCreateInstance(toggle).show()
-          dropdown_items_click()
-        }
+        if (!isDesktop()) return
+        closeOtherDropdowns()
+        getDropdown(toggle).show()
+        dropdown_items_click()
+      }
+
+      function closeMenu() {
+        if (isDesktop()) getDropdown(toggle).hide()
       }
 
       function scheduleClose() {
-        closeTimer = setTimeout(function () {
-          if (window.innerWidth >= 1024) {
-            bootstrap.Dropdown.getOrCreateInstance(toggle).hide()
-          }
-        }, 300)
+        closeTimer = setTimeout(closeMenu, HOVER_CLOSE_DELAY)
       }
 
       item.addEventListener('mouseenter', openMenu)
