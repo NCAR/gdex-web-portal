827c3ffea9a726fdc17287710d7d4b7289e03e24
rda-dattore <dattore@ucar.edu>
2026-07-31 10:44:56 -0600

    update start()

--- metaman_lite/views.py
+++ metaman_lite/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from metaman.models import MetamanPage
+from wagtail.models import Page
 
 
 def start(request):
-    return HttpResponse("metaman-lite")
+    qs = Page.objects.type(MetamanPage)
+    return render(request, "metaman_lite/start.html",
+                  {'title': qs[0].get_context(request)['page']['title']})
