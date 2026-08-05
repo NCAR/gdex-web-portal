ed6744edc60f7541d8cd87b3778dd27bf34ed33a
rda-dattore <dattore@ucar.edu>
2026-08-05 16:12:22 -0600

    accept token in start()

--- metaman_lite/views.py
+++ metaman_lite/views.py
@@ -3,6 +3,6 @@ from metaman.models import MetamanPage
 from wagtail.models import Page
 
 
-def start(request):
+def start(request, token):
     qs = Page.objects.type(MetamanPage).live().specific()
     return render(request, "metaman_lite/start.html", {'title': qs[0].title})
